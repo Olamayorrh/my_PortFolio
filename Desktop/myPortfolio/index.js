85009dd46a73825786785b577da4f2dedc12dc1e
olamayorrh <olamayorrh@gmail.com>
2024-02-28 08:19:00 +0100

    updated project

--- Desktop/myPortfolio/index.js
+++ Desktop/myPortfolio/index.js
@@ -1,19 +1,81 @@
 let harmbug = document.querySelector(".harmbug");
 
-let menu = document.querySelectorAll('.menu')
+let link = document.querySelectorAll(".navLink");
 
-//console.log(menu);
+let menu = document.querySelector('.nav');
+
+console.log(menu);
 
  harmbug.addEventListener('click', ()=>{
    
 
     harmbug.classList.toggle("active");
     menu.classList.toggle("active");
- })
+ });
 
 document.querySelectorAll(".menuLink").forEach(n => {
     n.addEventListener("click",()=>{
         harmbug.classList.remove("active");
         menu.classList.remove("active");
-    })
-})
+    });
+});
+
+
+let store = [];
+
+let  name = document.getElementById("uname");
+let email = document.getElementById("umail");
+let number = document.getElementById("num");
+let message = document.getElementById("mssg");
+let button = document.getElementById("btn");
+
+
+
+
+let contact=(e)=>{
+ 
+     e.preventDefault();
+     let usersDetails = {
+        name: name.value,
+        mail : email.value,
+        number : number.value,
+        message : message.value,
+        };
+
+        
+        
+            if(email.value && message.value ){
+              store.push(usersDetails)
+              console.log(store);
+                  if(store.length > 0){
+                     store.forEach((name)=>{
+                      console.log(name.mail);
+                      localStorage.setItem("user",JSON.stringify(store));
+                      
+                    
+                   alert("message sent successfully")
+                   return
+                        
+                  
+            
+                      
+                    })
+                  }
+
+                     
+
+            } else{
+                alert("kindly fill the boxes")
+                }
+                
+                document.querySelector('form').reset();   
+     
+}
+
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+   
+  btn.addEventListener('click',contact);
+ 
+});
